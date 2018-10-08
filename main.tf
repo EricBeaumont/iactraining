b786e1bae3ffc8be36e115992738bb4078339560
eric <toto@eh.com>
2018-10-08 15:40:51 +0200

    Ajout subnet

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ resource "aws_vpc" "vpc_eric" {
     CostCenter = "mycostcenter"
   }
 }
+
+resource "aws_subnet" "subnet_eric" {
+  vpc_id     = "${aws_vpc.vpc_eric.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name       = "subnet_eric"
+    CostCenter = "mycostcenter"
+  }
+}
